66d4351d5ccd18ed46f0185ec3e40e0904b2b762
Vaibhav <vabhfabh@gmail.com>
2016-04-03 05:15:39 -0500

    Initial Commit

---
+++ GroupChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupChatDemo
+//
+//  Created by Vaibhav Mistry on 4/3/16.
+//  Copyright © 2016 Vaibhav Mistry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
